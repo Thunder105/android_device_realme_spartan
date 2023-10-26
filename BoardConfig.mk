177d8eec2dc870d46a9d7e58aa5cd9c1caa3c178
yograjfire18 <yograjfire18@gmail.com>
2023-10-26 18:30:27 +0530

    spartan: Track compatibility matrix from source

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -76,7 +76,8 @@ TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/oplus:libudfps_extension.oplus
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(DEVICE_PATH)/device_framework_matrix.xml
+    $(DEVICE_PATH)/device_framework_matrix.xml \
+    vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 ODM_MANIFEST_FILES := $(DEVICE_PATH)/manifest_odm.xml
