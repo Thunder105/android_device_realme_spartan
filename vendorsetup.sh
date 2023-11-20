048ba3d444a093380cf929f1a61e3bb417e9846f
yograjfire18 <yograjfire18@gmail.com>
2023-11-20 09:16:01 +0530

    spartan: Add vendorsetup to clone additional repos - clone viper4fx - clone opluscamera

---
+++ vendorsetup.sh
@@ -0,0 +1,5 @@
+# Viper4FX
+git clone https://github.com/swiitch-OFF-Lab/packages_apps_ViPER4AndroidFX --depth 1 packages/apps/ViPER4AndroidFX
+
+# OplusCamera
+git clone https://gitlab.com/yograjsingh-cmd/proprietary_vendor_oplus_camera.git -b lineage-20 --depth 1 vendor/oplus/camera
